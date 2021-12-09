2894c3badb014d6d14ebd999a74c81f10524e1e4
trabol <f.trabol@gmail.com>
2021-12-08 21:12:36 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/trabol/golang-plan
+
+go 1.17
---
+++ utils.go
@@ -0,0 +1,8 @@
+package utils
+
+import "fmt"
+
+//prmiera letra en mayuscula es publica
+func HelloWorld() {
+	fmt.Println("hole mundo")
+}
